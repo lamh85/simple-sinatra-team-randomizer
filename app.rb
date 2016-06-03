917813b298af33be1629ed13c6183b11a203dbb0
david <David.Leehr@gmail.com>
2016-06-03 09:09:41 -0700

    started working on team assignment (still unfinished), added main.css

--- app.rb
+++ app.rb
@@ -12,15 +12,29 @@ get '/' do
 end
 
 post '/' do
+  @names = params[:names]
+  @num_of_teams = params[:num_of_teams]
   # Checks whether textarea is empty
-  if params[:names].strip.empty?
+  if @names.strip.empty?
     @error = "You haven't entered any names!"
     erb :index, layout: :app_layout
   # Checks the number of teams input for non-digit characters or emptiness
-  elsif !(params[:num_of_teams] =~ /\D/).nil? || params[:num_of_teams].strip.empty?
+  elsif !(@num_of_teams =~ /\D/).nil? || @num_of_teams.strip.empty? || @num_of_teams.to_i > @names.strip.length
     @error = "Invalid input for number of teams!"
     erb :index, layout: :app_layout
+  # Assign teams if checks have been passed  
   else
+    @names = @names.split(',')
+    @num_of_teams = @num_of_teams.to_i
+    # Initialize empty teams
+    1.upto(@num_of_teams) do |x|
+      session[:teams]["Team #{x}".to_sym] = 0
+    end
+    # Distribute names among teams in a random fashion
+#    while @names.length > 0
+#      
+#    end
+    p session[:teams]
     erb :index, layout: :app_layout
   end
 
